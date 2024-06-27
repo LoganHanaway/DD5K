38f61276669591703f0df76ba10e14dc7fafa09e
Logan Hanaway <lthanaway14@gmail.com>
2024-06-27 15:38:14 -0500

    preload LCP and X'd unused JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,101 @@
-
-
 (function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $wrapper = $("#page-wrapper"),
+    $banner = $("#banner"),
+    $header = $("#header");
 
-	var $window = $(window),
-		$body = $('body'),
-		$wrapper = $('#page-wrapper'),
-		$banner = $('#banner'),
-		$header = $('#header');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: [null, "480px"],
+  });
 
-	// Breakpoints.
-	breakpoints({
-		xlarge: ['1281px', '1680px'],
-		large: ['981px', '1280px'],
-		medium: ['737px', '980px'],
-		small: ['481px', '736px'],
-		xsmall: [null, '480px']
-	});
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-	$window.on('load', function () {
-		window.setTimeout(function () {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
+  // Mobile?
+  if (browser.mobile) $body.addClass("is-mobile");
+  else {
+    breakpoints.on(">medium", function () {
+      $body.removeClass("is-mobile");
+    });
 
-	// Mobile?
-	if (browser.mobile)
-		$body.addClass('is-mobile');
-	else {
+    breakpoints.on("<=medium", function () {
+      $body.addClass("is-mobile");
+    });
+  }
 
-		breakpoints.on('>medium', function () {
-			$body.removeClass('is-mobile');
-		});
+  // Scrolly.
+  $(".scrolly").scrolly({
+    speed: 1500,
+    offset: $header.outerHeight(),
+  });
 
-		breakpoints.on('<=medium', function () {
-			$body.addClass('is-mobile');
-		});
+  // Menu.
+  $("#menu")
+    .append('<a href="#menu" class="close"></a>')
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: "right",
+      target: $body,
+      visibleClass: "is-menu-visible",
+    });
 
-	}
+  // Header.
+  if ($banner.length > 0 && $header.hasClass("alt")) {
+    $window.on("resize", function () {
+      $window.trigger("scroll");
+    });
 
-	// Scrolly.
-	$('.scrolly')
-		.scrolly({
-			speed: 1500,
-			offset: $header.outerHeight()
-		});
-
-	// Menu.
-	$('#menu')
-		.append('<a href="#menu" class="close"></a>')
-		.appendTo($body)
-		.panel({
-			delay: 500,
-			hideOnClick: true,
-			hideOnSwipe: true,
-			resetScroll: true,
-			resetForms: true,
-			side: 'right',
-			target: $body,
-			visibleClass: 'is-menu-visible'
-		});
-
-	// Header.
-	if ($banner.length > 0
-		&& $header.hasClass('alt')) {
-
-		$window.on('resize', function () { $window.trigger('scroll'); });
-
-		$banner.scrollex({
-			bottom: $header.outerHeight() + 1,
-			terminate: function () { $header.removeClass('alt'); },
-			enter: function () { $header.addClass('alt'); },
-			leave: function () { $header.removeClass('alt'); }
-		});
-
-	}
-
+    $banner.scrollex({
+      bottom: $header.outerHeight() + 1,
+      terminate: function () {
+        $header.removeClass("alt");
+      },
+      enter: function () {
+        $header.addClass("alt");
+      },
+      leave: function () {
+        $header.removeClass("alt");
+      },
+    });
+  }
 })(jQuery);
 
-// Photogallery lightbox //
+// // Photogallery lightbox //
+// // currently unused
 
-// Get all the gallery images and add a click event listener to each one
-const images = document.querySelectorAll('.gallery-image');
-images.forEach(image => {
-	image.addEventListener('click', () => {
-		// Create a new image element to display the enlarged photo
-		const enlargedImage = new Image();
-		enlargedImage.src = image.src;
-		enlargedImage.classList.add('enlarged-image');
+// // Get all the gallery images and add a click event listener to each one
+// const images = document.querySelectorAll('.gallery-image');
+// images.forEach(image => {
+// 	image.addEventListener('click', () => {
+// 		// Create a new image element to display the enlarged photo
+// 		const enlargedImage = new Image();
+// 		enlargedImage.src = image.src;
+// 		enlargedImage.classList.add('enlarged-image');
 
-		// Create a div to hold the enlarged photo and add it to the page
-		const lightbox = document.createElement('div');
-		lightbox.classList.add('lightbox');
-		lightbox.appendChild(enlargedImage);
-		document.body.appendChild(lightbox);
+// 		// Create a div to hold the enlarged photo and add it to the page
+// 		const lightbox = document.createElement('div');
+// 		lightbox.classList.add('lightbox');
+// 		lightbox.appendChild(enlargedImage);
+// 		document.body.appendChild(lightbox);
 
-		// Add a click event listener to the lightbox to remove it when clicked
-		lightbox.addEventListener('click', () => {
-			document.body.removeChild(lightbox);
-		});
-	});
-});
+// 		// Add a click event listener to the lightbox to remove it when clicked
+// 		lightbox.addEventListener('click', () => {
+// 			document.body.removeChild(lightbox);
+// 		});
+// 	});
+// });
